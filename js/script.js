5b8e8c49472af90e5791b3e67e6904a8ec8fdc4a
LloydC <lloydchambrier@gmail.com>
2025-07-02 11:41:15 +0200

    🔨Ajout de la logique js pour un bouton qui va filtrer les boursiers dans la liste

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ const etudiants = [
 const studentList = document.getElementById('student-list');
 
 //2. Créer une fonction pour générer le HTML de la liste des étudiants
-function generateStudentList() {
+function generateStudentList(list) {
     let html = '<ul>';
-    etudiants.forEach(etudiant => {
+    list.forEach(etudiant => {
         html += `<li>${etudiant.nom} - Age: ${etudiant.age}, Moyenne: ${etudiant.moyenne}, Programme: ${etudiant.programme}, Boursier: ${etudiant.boursier ? 'Oui' : 'Non'}</li>`;
         // html = html + '<li>' + etudiant.nom + ' - Age: ' + etudiant.age + ', Moyenne: ' + etudiant.moyenne + ', Programme: ' + etudiant.programme + ', Boursier: ' + (etudiant.boursier ? 'Oui' : 'Non') + '</li>';
         // html = '<ul>' + '<li>' + etudiant.nom + ' - Age: ' + etudiant.age + ', Moyenne: ' + etudiant.moyenne + ', Programme: ' + etudiant.programme + ', Boursier: ' + (etudiant.boursier ? 'Oui' : 'Non') + '</li>';
@@ -22,4 +22,10 @@ function generateStudentList() {
 }
 
 //3. Insérer le HTML généré dans l'élément de la page
-studentList.innerHTML = generateStudentList();
+studentList.innerHTML = generateStudentList(etudiants);
+
+// 1. Capturer l'element du bouton et on va lui rajouter une fonctionalite pour filtrer la liste d'etudiants
+document.getElementById('filter-boursiers').addEventListener('click', () => {
+    const boursiers = etudiants.filter(student => student.boursier === true);
+    generateStudentList(boursiers); // Rendre les boursiers sur la page
+});
