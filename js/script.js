a97999e89f4dec42a47518255b7da985cc65a44b
LloydC <lloydchambrier@gmail.com>
2025-07-02 10:46:02 +0200

    🔧Ajout du fichier script.js dans index.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Malaika Web Demo Loaded');
+const etudiants = [
+    { nom: "Jean Dupuis", age: 20, moyenne: 15.5, programme: "Informatique", boursier: false },
+    { nom: "Marie Lefevre", age: 22, moyenne: 17.2, programme: "Droit", boursier: true },
+    { nom: "Pierre Martin", age: 21, moyenne: 12.8, programme: "Mathématiques", boursier: false },
+    { nom: "Sophie Durand", age: 19, moyenne: 14.9, programme: "Informatique", boursier: true },
+    { nom: "Lucie Bernard", age: 23, moyenne: 16.3, programme: "Physique", boursier: false }
+];
